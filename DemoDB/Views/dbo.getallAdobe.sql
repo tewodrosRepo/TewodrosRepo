0a9cfff5f9d36f61a59740593e8eab6c6b352eb8
tewodrosRepo <tewodros_seifu@yahoo.com>
2019-08-17 17:16:08 -0400

    New View to get all Adope list

---
+++ DemoDB/Views/dbo.getallAdobe.sql
@@ -0,0 +1,17 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+/****** Script for SelectTopNRows command from SSMS  ******/
+
+CREATE VIEW [dbo].[getallAdobe]
+AS
+SELECT [Abode_ID]
+      ,[Person_id]
+      ,[Address_id]
+      ,[TypeOfAddress]
+      ,[Start_date]
+      ,[End_date]
+      ,[ModifiedDate]
+  FROM [Customers].[Customer].[Abode]
+GO
